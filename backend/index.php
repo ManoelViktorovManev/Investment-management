54eb5ca6e897626c7fcbbda4159fad5ba38fd8f1
ManoelViktorovManev <mani.manev@gmail.com>
2025-06-03 23:43:16 +0300

    adding a header into the php file so now can handle income request from react api

--- backend/index.php
+++ backend/index.php
@@ -1,4 +1,12 @@
 <?php
+// Allow requests from localhost:3000
+header("Access-Control-Allow-Origin: http://localhost:3000");
+// Optional: allow cookies and other credentials
+header("Access-Control-Allow-Credentials: true");
+// Optional: allow specific methods
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+// Optional: allow specific headers
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 require __DIR__ . '/vendor/autoload.php';
 
 use App\Core\App;
