f36e8d2a4262e861a15f3f84a5a2479670ff8e97
ManoelViktorovManev <mani.manev@gmail.com>
2025-07-11 17:33:23 +0300

    Adding MariaDB database into the start.bat file

--- start.bat
+++ start.bat
@@ -1,2 +1,10 @@
+@echo off
+
+:: Start MariaDB server
+start "" "C:\Program Files\MariaDB 11.8\bin\mysqld.exe"
+
+:: Start frontend (React/Node)
 start cmd /k "cd interface && set NODE_OPTIONS=--openssl-legacy-provider && npm start"
+
+:: Start PHP backend
 start cmd /k "cd backend && php -S localhost:8080"
