6647833eded646a5c95eae9bd86e38c9c0fcefcc
ManoelViktorovManev <mani.manev@gmail.com>
2025-07-29 21:42:07 +0300

    Adding method for calling the TransactionHistory

--- backend/controller/TransactionHistoryController.php
+++ backend/controller/TransactionHistoryController.php
@@ -7,12 +7,10 @@ use App\Model\StockTransactions;
 use App\Model\Stock;
 use App\Model\Portfolio;
 use App\Core\DbManipulation;
+use App\Core\Route;
 
 class TransactionHistoryController extends BaseController
 {
-
-
-
 
     public function createNewStockTransactionsInstance(Stock $stock, Portfolio $portfolio, float $stockQuantity, float $stockPrice, string $transactionDate, string $transactionType)
     {
@@ -27,5 +25,13 @@ class TransactionHistoryController extends BaseController
         $db = new DbManipulation();
         $db->add($stockTransaction);
         $db->commit();
+    }
+
+    #[Route('/getTransactionHistory/{id}')]
+    public function getTransactionHistory($id)
+    {
+        $stockTransaction = new StockTransactions();
+        $array = $stockTransaction->query()->limit(10, $id)->all();
+        return $this->json($array);
     }
 }
